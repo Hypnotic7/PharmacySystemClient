fb4f9edc8088ae0658963a2515edd2500455f5c7
Pascal McKeon <pascalmckeon@gmail.com>
2017-10-11 18:02:30 +0100

    UI

---
+++ PharmacySystemClient/Loginpanel.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PharmacySystemClient
+{
+    /// <summary>
+    /// Interaction logic for Loginwindow.xaml
+    /// </summary>
+    public partial class Loginwindow : Window
+    {
+        public Loginwindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var username = GetUsername();
+            var password = GetPassword();
+            bool isValid = ValidateUsernameAndPassword(username, password);
+
+            if (isValid == true)
+             {
+                  Window mainMenuWindow = new MainMenu();
+                  mainMenuWindow.Show();
+                  this.Close();
+             }
+            else
+            {
+                  MessageBox.Show("Invalid username or password", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+           
+        }
+
+        private string GetUsername()
+        {
+            return UsernameTextField.Text;
+        }
+        private string GetPassword()
+        {
+            return PasswordField.Password;
+        }
+
+        private bool ValidateUsernameAndPassword(string username,string password)
+        {
+            if (username.Length > 0 && password.Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PharmacySystemClient/MainMenuPanel.xaml.cs
+++ PharmacySystemClient/MainMenuPanel.xaml.cs
@@ -10,19 +10,29 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PharmacySystemClient
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainMenu.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainMenu : Window
     {
-        public MainWindow()
+        public MainMenu()
         {
             InitializeComponent();
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender.Equals("Process Order"))
+            {
+                OrderPanel orderPanel = new OrderPanel();
+                orderPanel.InitializeComponent();
+               
+
+            }  
         }
     }
 }
